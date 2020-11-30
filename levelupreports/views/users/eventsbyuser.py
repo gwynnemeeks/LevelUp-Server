bb6c5f37447fa9694848e2c8141f8ae73cda1b5a
Gwynne Meeks <meeks.gwynne@gmail.com>
2020-11-30 10:52:39 -0600

    wip

--- levelupreports/views/users/eventsbyuser.py
+++ levelupreports/views/users/eventsbyuser.py
@@ -42,8 +42,9 @@ def userevent_list(request):
             #         "events": [
             #             {
             #                 "id": 5,
-            #                 "date": "2020-12-23",
+            #                 "day": "2020-12-23",
             #                 "time": "19:00",
+            #                 "location": "Chez Meeks"
             #                 "game_name": "Fortress America"
             #             }
             #         ]
@@ -51,3 +52,38 @@ def userevent_list(request):
             # }
 
             events_by_user = {}
+
+            for row in dataset:
+                # Create an Event instance and set its properties
+                event = Event()
+                event.day = row['day']
+                event.time = row['time']
+                event.location = row['location']
+                event.game_id = row['game_id'] # this row might give you problems
+
+                # Store the user's id
+                uid = row["user_id"]
+
+                # If the user's id is already a key in the dictionary ...
+                if uid in events_by_user:
+
+                    #Add the current event to the `events` list for it
+                    events_by_user[uid]['events'].append(event)
+
+                else:
+                    # otherwise, create the key and dictionary value
+                    events_by_user[uid] = {}
+                    events_by_user[uid]["id"] = uid
+                    events_by_user[uid]["full_name"] = row ["full_name"]
+                    events_by_user[uid]["events"] = [event]
+
+        # Get only the values from the dictionary and create a list from them
+        list_of_users_with_events = events_by_user.values()
+
+        # Specify the Django template and provide data context
+        template = 'users/lost_with_events.html'
+        context = {
+            'userevent_list': list_of_users_with_events
+        }
+
+        return render(request, template, context)
