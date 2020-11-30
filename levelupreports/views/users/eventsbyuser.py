571098ad2d1b52e77ddb294e78a3a57fb6bfb787
Gwynne Meeks <meeks.gwynne@gmail.com>
2020-11-30 10:03:48 -0600

    wip

--- levelupreports/views/users/eventsbyuser.py
+++ levelupreports/views/users/eventsbyuser.py
@@ -15,11 +15,39 @@ def userevent_list(request):
             # Query for all events, with related user info
             db_cursor.execute("""
                 SELECT
-                    e.id event_id
-                    e.day
-                    e.time
-                    e.location
-                    e.game_id 
+                    e.id,
+                    e.day,
+                    e.time,
+                    e.location,
+                    e.game_id,
                     u.id user_id,
                     u.first_name || ' ' || u.last_name AS full_name
+                FROM
+                    levelupapi_event e
+                JOIN
+                    levelupapi_gamer gr on e.gamer_id = gr.id
+                JOIN
+                    auth_user u on gr.user_id = u.id
             """)
+
+            dataset = db_cursor.fetchall()
+
+            # Take the flat data from the database, and build the
+            # following data structure for each gamer.
+            #
+            # {
+            #     1: {
+            #         "organizer_id": 1,
+            #         "full_name": "Molly Ringwald",
+            #         "events": [
+            #             {
+            #                 "id": 5,
+            #                 "date": "2020-12-23",
+            #                 "time": "19:00",
+            #                 "game_name": "Fortress America"
+            #             }
+            #         ]
+            #     }
+            # }
+
+            events_by_user = {}
